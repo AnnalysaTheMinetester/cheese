ef7bd2469fe5f74111d3dc8e85846bd7caab43b2
Annalysa <100380216+AnnalysaTheMinetester@users.noreply.github.com>
2024-12-01 09:48:36 +0100

    Solved #14
    Fondue returns the correct item when hunger_ng is present

--- items.lua
+++ items.lua
@@ -454,5 +454,5 @@ if cheese.hunger_ng then
 	hunger_ng.add_hunger_data("cheese:caciocavallo", { satiates = 8, heals = 0, timeout = 0 })
 	hunger_ng.add_hunger_data("cheese:scamorza", { satiates = 4, heals = 0, timeout = 0 })
 	hunger_ng.add_hunger_data("cheese:smoked_scamorza", { satiates = 7, heals = 0, timeout = 0 })
-	hunger_ng.add_hunger_data("cheese:fondue", { satiates = 8, heals = 1, returns = "default:copper_ingot", timeout = 3 })
+	hunger_ng.add_hunger_data("cheese:fondue", { satiates = 8, heals = 1, returns = "cheese:fondue_pot", timeout = 3 })
 end
